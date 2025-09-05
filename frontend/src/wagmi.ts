72530bc529c93a0221bf14c7484116da7936f95b
zyhuang <zyhuang@yishion.net>
2025-09-05 10:27:27 +0800

    文件结构优化，配置完善

---
+++ frontend/src/wagmi.ts
@@ -0,0 +1,45 @@
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+} from "wagmi/chains";
+import { http } from "wagmi";
+export const config = getDefaultConfig({
+  appName: "RainbowKit App",
+  //作用：连接钱包
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
+  ],
+  transports: {
+    [mainnet.id]: http(
+      `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`
+    ),
+    [polygon.id]: http(
+      `https://polygon-mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`
+    ),
+    [optimism.id]: http(
+      `https://optimism-mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`
+    ),
+    [arbitrum.id]: http(
+      `https://arbitrum-mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`
+    ),
+    [base.id]: http(
+      `https://base-mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`
+    ),
+    // 🚀 重点：强制指定 Sepolia 的 Infura 节点
+    [sepolia.id]: http(
+      `https://sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`
+    ),
+  },
+  ssr: true,
+});
